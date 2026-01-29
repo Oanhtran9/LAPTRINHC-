eb9798f2bb3e7ff8b5010a4e762d219e798c450f
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-01-29 19:23:44 +0700

    upadate bai 1, bai 2, bai 3

---
+++ Tuan_2/Tuan_2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Tuan_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập họ và tên: ");
+            string input = Console.ReadLine();
+
+            // bai1
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                string name = input.Trim();
+                string[] words = name.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string result = "";
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string ten = words[i].ToLower();
+                    ten = char.ToUpper(ten[0]) + ten.Substring(1).ToLower();
+                    result += ten + " ";
+                }
+
+                Console.WriteLine("Họ tên chuẩn hoá: " + result.Trim());
+                Console.WriteLine("Số từ trong tên : " + result.Length);
+            }
+            //bai3
+            if (input == null)
+            {
+                Console.WriteLine("Chuỗi không đối xứng");
+            }
+            else
+            {
+                string p = input.Replace(" ", "").ToLower();
+                bool doiXung = true;
+
+                for (int i = 0; i < p.Length / 2; i++)
+                {
+                    if (p[i] != p[p.Length - 1 - i])
+                    {
+                        doiXung = false;
+                        break;
+                    }
+                }
+
+                if (doiXung)
+                    Console.WriteLine("Chuỗi đối xứng");
+                else
+                    Console.WriteLine("Chuỗi không đối xứng");
+            }
+        }
+    }
+}
